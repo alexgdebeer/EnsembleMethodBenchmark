82aa14268bb1657aecb1f47d4cd3ef4224a9d390
Alex de Beer <adeb970@aucklanduni.ac.nz>
2023-05-03 10:50:34 +1200

    Clean up velocity field plot

--- examples/advection_diffusion/velocity_fields.jl
+++ examples/advection_diffusion/velocity_fields.jl
@@ -1,39 +1,18 @@
+using LaTeXStrings
+using Plots
 using Statistics
 
-using Plots
 Plots.gr()
+Plots.default(fontfamily="Computer Modern")
 
-# const xmin, Δx, xmax = -10.0, 2.0, 10.0
-# const ymin, Δy, ymax = 0.0, 2.0, 20.0
+const PLOTS_DIR = "plots/advection_diffusion"
 
-# xs = xmin:Δx:xmax 
-# ys = ymin:Δy:ymax 
+const TITLE_SIZE = 20
+const LABEL_SIZE = 16
+const SMALL_SIZE = 8
 
-# n_xs = length(xs)
-# n_ys = length(ys)
-
-# xs = xs' .* ones(n_xs) 
-# ys = ones(n_ys)' .* ys
-
-# us = zeros(n_xs, n_ys)
-
-# vs = collect(range(2, 0, n_ys)) .* ones(n_xs)'
-# vs[1:Int(ceil(n_ys/2)), :] .= 1.0
-
-# println(vs[:,1])
-
-# Plots.quiver(
-#     vec(xs), vec(ys),
-#     quiver=(vec(us), vec(vs))
-# )
-
-# Plots.xlabel!("x")
-# Plots.ylabel!("y")
-
-# Plots.savefig("quiver.pdf")
-
-const xmin, xmax, n_xs = -10.0, 10.0, 20
-const ymin, ymax, n_ys = 0.0, 10.0, 20
+const xmin, xmax, n_xs = -10.0, 10.0, 10
+const ymin, ymax, n_ys = 0.0, 10.0, 10
 
 xs = collect(range(xmin, xmax, n_xs))
 ys = collect(range(ymin, ymax, n_ys))
@@ -46,19 +25,20 @@ ys = ys .* ones(n_xs)'
 us = ones(Int(n_xs/2))' .* -collect(range(-1, 1, n_ys))
 vs = ones(n_ys) .* collect(range(-1, 1, Int(n_xs/2)))'
 
-us = 0.5.*[us -us]
-vs = 0.5.*[vs -vs]
+us = 1.0.*[us -us]
+vs = 1.0.*[vs -vs]
 
 Plots.quiver(
     vec(xs), vec(ys),
     quiver=(vec(us), vec(vs)),
-    aspect_ratio=:equal
+    aspect_ratio=:equal, arrow = :closed
 )
 
 Plots.xlims!(xmin, xmax)
 Plots.ylims!(ymin, ymax)
 
-Plots.xlabel!("x")
-Plots.ylabel!("y")
+Plots.title!("Velocity Field", fontsize=TITLE_SIZE)
+Plots.xlabel!(L"x", fontsize=LABEL_SIZE)
+Plots.ylabel!(L"y", fontsize=LABEL_SIZE)
 
-Plots.savefig("quiver.pdf")
+Plots.savefig("$PLOTS_DIR/velocity_field.pdf")
