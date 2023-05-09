a70572b64353566761861858592f7b580ca9ab64
Alex de Beer <adeb970@aucklanduni.ac.nz>
2023-05-10 11:48:48 +1200

    Tidy up MONOD EnRML script

--- examples/monod/monod_enrml.jl
+++ examples/monod/monod_enrml.jl
@@ -1,4 +1,4 @@
-"""Runs EnRML on the linear model."""
+"""Runs EnRML on the MONOD model."""
 
 include("monod_model.jl")
 include("../../plotting.jl")
@@ -7,16 +7,12 @@ include("../../sim_intensive_inference/sim_intensive_inference.jl")
 # Define the prior and ensemble size
 const π = SimIntensiveInference.GaussianPrior(MONODModel.μ_π, MONODModel.Γ_π)
 const β_0 = 0.5
-const N_e = 1000
+const N_e = 100
 
-θs = SimIntensiveInference.run_batch_enrml(
-    MONODModel.f, 
-    MONODModel.g,
-    π,  
-    MONODModel.YS_O, 
-    MONODModel.σ_ϵ, 
-    β_0,
-    N_e
+θs, ys = SimIntensiveInference.run_batch_enrml(
+    MONODModel.f, MONODModel.g, π, 
+    MONODModel.YS_O, MONODModel.σ_ϵ, 
+    β_0, N_e
 )
 
 Plotting.plot_approx_posterior(
@@ -26,4 +22,11 @@ Plotting.plot_approx_posterior(
     "MONOD: EnRML Posterior",
     "$(MONODModel.PLOTS_DIR)/enrml/enrml_posterior.pdf";
     caption="Ensemble size: $N_e."
+)
+
+Plotting.plot_monod_posterior_predictions(
+    MONODModel.XS, ys, 
+    MONODModel.XS_O, MONODModel.YS_O, 
+    "MONOD: EnRML Posterior Predictions",
+    "$(MONODModel.PLOTS_DIR)/enrml/enrml_posterior_predictions.pdf"
 )
