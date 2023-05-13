a2a5147d34a886a15849c201005f396120342956
Alex de Beer <adeb970@aucklanduni.ac.nz>
2023-05-13 12:13:52 +1200

    Add ES-MDA example for nonlinear model

---
+++ examples/simple_nonlinear/es_mda.jl
@@ -0,0 +1,27 @@
+"""Runs the ensemble smoother with multiple data assimilation."""
+
+using LaTeXStrings
+
+include("simple_nonlinear_model.jl")
+include("../../plotting.jl")
+include("../../sim_intensive_inference/sim_intensive_inference.jl")
+
+const π = SimIntensiveInference.GaussianPrior(SimpleNonlinear.μ_π, SimpleNonlinear.Γ_π)
+const αs = [16.0 for _ ∈ 1:16]
+const N_e = 10_000
+
+θs = SimIntensiveInference.run_es_mda(
+    SimpleNonlinear.f, SimpleNonlinear.g, π, 
+    vec(SimpleNonlinear.YS_O), SimpleNonlinear.σ_ϵ, 
+    αs, N_e
+)
+
+Plotting.plot_nonlinear_approx_posterior(
+    vec(θs), 
+    SimpleNonlinear.θS, 
+    SimpleNonlinear.POST, 
+    SimpleNonlinear.θS_T[1],
+    "ES-MDA Posterior",
+    "$(SimpleNonlinear.PLOTS_DIR)/es/es_mda_posterior_small_noise.pdf",
+    caption="Ensemble size: $N_e. Iterations: $(length(αs)). "*L"\sigma_\epsilon"*": $(SimpleNonlinear.σ_ϵ)."
+)
