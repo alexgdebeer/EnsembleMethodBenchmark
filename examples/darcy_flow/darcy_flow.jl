5a539e08aea73e2e756a6cf5976d7c891b842c0c
Alex de Beer <adeb970@aucklanduni.ac.nz>
2023-05-17 22:05:52 +1200

    Clean up Darcy flow example and plot some pressure and permeability fields

--- examples/darcy_flow/darcy_flow.jl
+++ examples/darcy_flow/darcy_flow.jl
@@ -8,17 +8,23 @@ using NonlinearSolve
 
 using Distributions 
 using LinearAlgebra
-using Random; Random.seed!(0)
+using Statistics
+using Random; Random.seed!(1)
 
-using Plots
+import PyPlot
 
-Plots.gr()
-Plots.default(fontfamily="Computer Modern")
+PyPlot.rc("text", usetex=true)
+PyPlot.rc("font", family="serif")
+
+# using Plots
+
+# Plots.gr()
+# Plots.default(fontfamily="Computer Modern")
 
 const PLOTS_DIR = "plots/darcy_flow"
 
 const TITLE_SIZE = 20
-const LABEL_SIZE = 16
+const LABEL_SIZE = 14
 
 global ks
 
@@ -50,6 +56,9 @@ function k(x, y)
     return ks[i, j]
 end
 
+# Returns the flux on the top boundary 
+v(x) = 2.0
+
 xmin, Δx, xmax = 0.0, 0.05, 1.0
 ymin, Δy, ymax = 0.0, 0.05, 1.0
 tmin, tmax = 0.0, 1.0
@@ -57,47 +66,47 @@ tmin, tmax = 0.0, 1.0
 xs = xmin:Δx:xmax
 ys = ymin:Δy:ymax
 
+pxs = xmin:0.01:xmax
+pys = ymin:0.01:ymax
+
 n_xs = length(xs)
 n_ys = length(ys)
 
-pxs = xmin:0.01:xmax
-pys = ymin:0.01:ymax
+n_pxs = length(pxs)
+n_pys = length(pys)
 
 ModelingToolkit.@register_symbolic k(x,y)
 ModelingToolkit.@register_symbolic v(x)
 
+# Generate some permeability parameters
+Γ = exp_squared_cov(0.5, 0.1)
+d = MvNormal(Γ)
+
+ModelingToolkit.@parameters x y t
+ModelingToolkit.@variables u(..)
+
+∂x = ModelingToolkit.Differential(x)
+∂y = ModelingToolkit.Differential(y)
+∂t = ModelingToolkit.Differential(t)
+
+eq = ∂x(k(x, y)*∂x(u(x, y, t))) + ∂y(k(x, y)*∂y(u(x, y, t))) ~ ∂t(u(x, y, t))
+
+domains = [
+    x ∈ DomainSets.Interval(xmin, xmax), 
+    y ∈ DomainSets.Interval(ymin, ymax),
+    t ∈ DomainSets.Interval(tmin, tmax)
+]
+
+# Constant pressure on the bottom, Neumann conditions on all other sides
+bcs = [
+    u(x, y, tmin) ~ 0.0,
+    ∂x(u(xmin, y, t)) ~ 0.0, 
+    ∂x(u(xmax, y, t)) ~ 0.0, 
+    ∂y(u(x, ymax, t)) ~ v(x), 
+    u(x, ymin, t) ~ 0.0
+]
+
 function solve_darcy_pde()
-
-    ModelingToolkit.@parameters x y t
-    ModelingToolkit.@variables u(..)
-
-    ∂x = ModelingToolkit.Differential(x)
-    ∂y = ModelingToolkit.Differential(y)
-    ∂t = ModelingToolkit.Differential(t)
-
-    # Generate some permeability parameters
-    Γ = exp_squared_cov(1.0, 0.1)
-    d = MvNormal(Γ)
-
-    # Define flux on top boundary 
-    v(x) = 2.0
-
-    eq = ∂x(k(x, y)*∂x(u(x, y, t))) + ∂y(k(x, y)*∂y(u(x, y, t))) ~ ∂t(u(x, y, t))
-
-    # Constant pressure on the bottom, Neumann conditions on all other sides
-    bcs = [
-        u(x, y, tmin) ~ 0.0,
-        ∂x(u(xmin, y, t)) ~ 0.0, 
-        ∂x(u(xmax, y, t)) ~ 0.0, 
-        ∂y(u(x, ymax, t)) ~ v(x), 
-        u(x, ymin, t) ~ 0.0
-    ]
-
-    domains = [
-        x ∈ DomainSets.Interval(xmin, xmax), 
-        y ∈ DomainSets.Interval(ymin, ymax),
-        t ∈ DomainSets.Interval(tmin, tmax)
-    ]
 
     global ks = sample_perms(d)
 
@@ -115,13 +124,54 @@ function solve_darcy_pde()
 
     ps = reshape(state.u, (length(xs)-2, length(ys)-2))
 
-    return ps
+    return ks, ps
 
 end
 
-ps_1 = solve_darcy_pde()
-ps_2 = solve_darcy_pde()
-ps_3 = solve_darcy_pde()
+perms = Array{Float64}(undef, n_pxs, n_pys, 3)
+pressures = Array{Float64}(undef, n_xs-2, n_ys-2, 3)
 
-heatmap(pxs, pys, log.(ks), cmap=:viridis)
-heatmap(xs[2:end-1], ys[2:end-1], ps_3)
+for i ∈ 1:3
+    perms[:,:,i], pressures[:,:,i] = solve_darcy_pde()
+end
+
+logperms = log.(perms)
+logperm_min = minimum(logperms)
+logperm_max = maximum(logperms)
+pressure_min = minimum(pressures)
+pressure_max = maximum(pressures)
+
+fig, ax = PyPlot.subplots(2, 3, figsize=(8, 5))
+
+for col ∈ 1:3 
+
+    m1 = ax[1, col].pcolormesh(
+        pxs, pys, rotr90(logperms[:, :, col]), 
+        cmap=:viridis, vmin=logperm_min, vmax=logperm_max
+    )
+    
+    m2 = ax[2, col].pcolormesh(
+        xs[2:end-1], ys[2:end-1], rotr90(pressures[:, :, col]), 
+        cmap=:coolwarm, vmin=pressure_min, vmax=pressure_max
+    )
+
+    for row ∈ 1:2
+        ax[row, col].set_box_aspect(1)
+        ax[row, col].set_xticks([0, 1])
+        ax[row, col].set_yticks([0, 1])
+    end
+
+    PyPlot.colorbar(m1, fraction=0.046, pad=0.04, ax=ax[1, col])
+    PyPlot.colorbar(m2, fraction=0.046, pad=0.04, ax=ax[2, col])
+
+end
+
+ax[1, 1].set_ylabel("ln(Permeability) Fields", fontsize=LABEL_SIZE)
+ax[2, 1].set_ylabel("Pressure Fields", fontsize=LABEL_SIZE)
+
+PyPlot.suptitle("ln(Permeability) and Pressure Fields", fontsize=TITLE_SIZE)
+PyPlot.tight_layout()
+PyPlot.savefig("$PLOTS_DIR/darcy_flow.pdf")
+
+# heatmap(pxs, pys, rotl90(log.(ks_1)), cmap=:viridis)
+# heatmap(xs[2:end-1], ys[2:end-1], rotl90(ps_3))
