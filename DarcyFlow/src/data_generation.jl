c954ecd6fd6b03d36f36cbe500605f4aa32e34b6
alexgdebeer <adeb970@aucklanduni.ac.nz>
2023-05-24 19:53:41 +1200

    Move covariance and sampling functions into data generation

--- DarcyFlow/src/data_generation.jl
+++ DarcyFlow/src/data_generation.jl
@@ -1,3 +1,22 @@
+function exp_squared_cov(σ, γ, xs, ys)
+
+    # Generate vectors of x and y coordinates
+    cxs = vec([x for x ∈ xs, _ ∈ ys])
+    cys = vec([y for _ ∈ xs, y ∈ ys])
+
+    # Generate a matrix of distances between each set of coordinates
+    ds = (cxs .- cxs').^2 + (cys .- cys').^2
+
+    Γ = σ^2 * exp.(-(1/2γ^2) * ds) + 1e-6I
+
+    return Γ
+
+end
+
+function sample_perms(d, n_x, n_y) 
+    return reshape(rand(d), n_x, n_y)
+end
+
 function generate_data(xs, ys, bcs, p_dist, x_locs, y_locs, ϵ_dist)
 
     n_x = length(xs)
