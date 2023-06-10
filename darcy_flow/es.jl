5166f4d297e11cb00504ca74d7cba78b3c2f36a2
Alex de Beer <adeb970@aucklanduni.ac.nz>
2023-06-10 22:19:17 +1200

    Add ensemble smoother script

--- darcy_flow/es.jl
+++ darcy_flow/es.jl
@@ -3,18 +3,16 @@ using SimIntensiveInference
 include("problem_setup.jl")
 include("plotting.jl")
 
-# Define inflation factors
-αs = [16.0 for _ ∈ 1:16]
 n = 1000
 
-logps, us = run_es_mda(f, g, π, L, αs, n)
+logps, us = run_es(f, g, π, L, n)
 
 # Calculate the mean and standard deviation of the (log-)permeabilities
 μ_post = reshape(mean(logps[:,:,end], dims=2), g_c.nx, g_c.ny)
 σ_post = reshape(std(logps[:,:,end], dims=2), g_c.nx, g_c.ny)
 
-fname = "plots/darcy_flow/mda/mda_mean.pdf"
+fname = "plots/darcy_flow/es/es_mean.pdf"
 plot_μ_post(μ_post, logps_t, g_c, g_f, fname)
 
-fname = "plots/darcy_flow/mda/mda_stds.pdf"
+fname = "plots/darcy_flow/es/es_stds.pdf"
 plot_σ_post(σ_post, g_c, fname)
