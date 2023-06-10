1385215fec14cd63e50d906119983f443666bd3d
Alex de Beer <adeb970@aucklanduni.ac.nz>
2023-06-10 17:08:16 +1200

    Add RML script

--- darcy_flow/mda.jl
+++ darcy_flow/mda.jl
@@ -5,9 +5,9 @@ include("plotting.jl")
 
 # Define inflation factors
 αs = [16.0 for _ ∈ 1:16]
-N_e = 1000
+n = 1000
 
-logps, us = SimIntensiveInference.run_es_mda(f, g, π, L, αs, N_e)
+logps, us = run_es_mda(f, g, π, L, αs, n)
 
 # Calculate the mean and standard deviation of the (log-)permeabilities
 μ_post = reshape(mean(logps[:,:,end], dims=2), g_c.nx, g_c.ny)
