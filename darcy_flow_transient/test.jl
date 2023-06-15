48fba30ff077fd0f623659e8c3e98b0d784c0d73
alexgdebeer <adeb970@aucklanduni.ac.nz>
2023-06-15 23:19:15 +1200

    Update test script to compare steady state and time-varying solutions

--- darcy_flow_transient/test.jl
+++ darcy_flow_transient/test.jl
@@ -11,10 +11,10 @@ Random.seed!(16)
 
 xmin, xmax = 0.0, 1.0
 ymin, ymax = 0.0, 1.0
-tmax = 1.0
+tmax = 10.0
 
-Δx, Δy = 0.01, 0.01
-Δt = 0.01
+Δx, Δy = 0.02, 0.02
+Δt = 0.05
 
 g = construct_grid(xmin:Δx:xmax, ymin:Δy:ymax, tmax, Δt)
 
@@ -22,7 +22,8 @@ bcs = Dict(
     :x0 => BoundaryCondition(:x0, :neumann, (x, y) -> 0.0), 
     :x1 => BoundaryCondition(:x1, :neumann, (x, y) -> 0.0),
     :y0 => BoundaryCondition(:y0, :neumann, (x, y) -> 2.0), 
-    :y1 => BoundaryCondition(:y1, :dirichlet, (x, y) -> 0.0)
+    :y1 => BoundaryCondition(:y1, :dirichlet, (x, y) -> 0.0),
+    :t0 => BoundaryCondition(:t0, :initial, (x, y) -> 1.0)
 )
 
 σ, γ = 1.0, 0.10
@@ -32,20 +33,12 @@ logp_dist = MvNormal(Γ_p)
 logps = sample_perms(logp_dist, g.nx, g.ny)
 ps = exp.(logps)
 
-u0 = ones(g.nx, g.ny)
-u0 = vec(u0)
+us = solve_system(g, ps, bcs)
 
-us = zeros(g.nx, g.ny, g.nt+1)
-us[:,:,1] = u0
+g_ss = construct_grid(xmin:Δx:xmax, ymin:Δy:ymax)
+A = construct_A(g_ss, ps, bcs)
+b = construct_b(g_ss, ps, bcs)
 
-A = construct_A(g, ps, bcs)
-
-for t ∈ 1:g.nt 
-
-    b = construct_b(g, ps, bcs, us[:,:,t])
-    u = solve(LinearProblem(A, b))
-    us[:,:,t+1] = reshape(u[g.nu+1:end], g.nx, g.ny)
-
-end
+us_ss = solve(LinearProblem(A, b))
 
 println("Done!")
