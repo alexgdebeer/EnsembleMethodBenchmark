72163a5b4d14d82bf0a0e8a42ca68014b01f0513
alexgdebeer <adeb970@aucklanduni.ac.nz>
2023-06-20 16:22:02 +1200

    Add new ensemble method files for transient Darcy flow model

--- darcy_flow_transient/lm_enrml.jl
+++ darcy_flow_transient/lm_enrml.jl
@@ -3,9 +3,12 @@ using SimIntensiveInference
 include("setup_ss_channel.jl")
 
 αs = [16.0 for _ ∈ 1:16]
-n = 1000
+n = 100
 
-θs, us = run_es_mda(f, g, p, L, αs, n)
+γ = 10
+i_max = 16
+
+θs, us, Ss, λs = run_lm_enrml(f, g, p, L, γ, i_max, n)
 
 logps = θs[6:end,:,:]
 μ_post = reshape(mean(logps[:,:,end], dims=2), grid.nx, grid.ny)
