4e1c434016d55b716bd348cdf979e2d5a4db0666
alexgdebeer <adeb970@aucklanduni.ac.nz>
2023-07-03 11:29:29 +1200

    Tidy up well test file

--- darcy_flow_transient/well_test.jl
+++ darcy_flow_transient/well_test.jl
@@ -1,93 +1,12 @@
+using LaTeXStrings
 using Random: seed!
 using SimIntensiveInference
+
 include("setup/setup.jl")
 
 seed!(16)
 
-function normalising_constant(g::Grid, x::Real, y::Real, r::Real)::Real
-
-    a = 0.0
-
-    for i ∈ 1:g.nx, j ∈ 1:g.ny
-
-        r_sq = (g.xs[i] - x)^2 + (g.ys[j] - y)^2
-    
-        if r_sq < r^2
-            a += exp(-1/(r^2-r_sq))
-        end
-    
-    end
-
-    return a
-
-end
-
-struct BumpWell
-
-    x::Real
-    y::Real
-    r::Real
-    t0::Real
-    t1::Real
-    q::Real
-    a::Real
-    
-    function BumpWell(
-        g::Grid, 
-        x::Real, 
-        y::Real, 
-        r::Real,
-        t0::Real,
-        t1::Real, 
-        q::Real
-    )
-
-        a = normalising_constant(g, x, y, r)
-        return new(x, y, r, t0, t1, q, a)
-    
-    end
-
-end
-
-struct DeltaWell 
-
-    x::Real 
-    y::Real
-    t0::Real 
-    t1::Real
-    q::Real
-
-end
-
-function well_rate(w::BumpWell, x::Real, y::Real, t::Real)::Real
-
-    if t < w.t0 || t > w.t1
-        return 0.0
-    end
-
-    r_sq = (x-w.x)^2 + (y-w.y)^2
-
-    if r_sq ≥ w.r^2
-        return 0.0
-    end
-
-    return w.q * exp(-1/(w.r^2-r_sq)) / w.a
-
-end
-
-function well_rate(w::DeltaWell, x::Real, y::Real, t::Real)::Real 
-
-    if t < w.t0 || t > w.t1
-        return 0.0
-    end
-
-    if abs(w.x - x) ≤ 1e-8 && abs(w.y - y) ≤ 1e-8
-        return w.q 
-    end
-
-    return 0.0
-    
-end
+animate = true
 
 xmin, Δx, xmax = 0.0, 10.0, 1000.0
 ymin, Δy, ymax = 0.0, 10.0, 1000.0
@@ -134,7 +53,6 @@ wells = [
 #     DeltaWell(500, 500, 0, 60, q_is)
 # ]
 
-# Define forcing function 
 q(x, y, t) = sum(well_rate(w, x, y, t) for w ∈ wells)
 
 σ, γx, γy = 0.5, 100, 100
@@ -147,27 +65,41 @@ logps = reshape(rand(p), grid.nx, grid.ny)
 ps = 10.0 .^ logps
 
 us = @time solve(grid, ps, bcs, q)
-us ./= 1.0e6
 
-anim = @animate for i ∈ axes(us, 3)
+if animate
 
-    plot(
-        heatmap(
-            grid.xs, grid.ys, us[:,:,i]', 
-            clims=extrema(us[2:end-1,2:end-1,:]), 
-            cmap=:turbo, 
-            aspect_ratio=:equal,
-            colorbar_title="Pressure",
-            title="t = $(grid.ts[i])",
-            titlefontsize=32,
-            colorbar_titlefontsize=20,
-            tickfontsize=16
-            # legend=:none
-        ),
-        #axis=([], false), 
-        size=(700, 700)
-    )
+    # Rescale pressures and extract pressures at well of interest
+    us ./= 1.0e6
+    well_us = us[21,22,:]
+
+    anim = @animate for i ∈ axes(us, 3)
+
+        plot(
+            heatmap(
+                grid.xs, grid.ys, us[:,:,i]', 
+                clims=extrema(us[2:end-1, 2:end-1, :]), 
+                cmap=:turbo, 
+                size=(500, 500),
+                title="Reservoir pressure vs time",
+                xlabel=L"x \, \textrm{(m)}",
+                ylabel=L"y \, \textrm{(m)}"
+            ),
+            plot(
+                well_us[1:i], 
+                size=(500, 500), 
+                xlims=(0, tmax),
+                ylims=extrema(well_us),
+                xlabel="Day",
+                ylabel="Pressure (MPa)",
+                title="Pressure in well at (200, 200)",
+                legend=:none
+            ),
+            size=(1000, 400),
+            margin=5Plots.mm
+        )
+
+    end
+
+    gif(anim, "pressure_plots.gif", fps=4)
 
 end
-
-gif(anim, "anim.gif", fps=4)
