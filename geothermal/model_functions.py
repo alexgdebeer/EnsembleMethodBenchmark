656f81dac764d4ab6aa2c8a3caaab345e8738c2d
alexgdebeer <adeb970@aucklanduni.ac.nz>
2023-07-07 19:46:23 +1200

    Add functions for generating models and processing model output

--- geothermal/model_functions.py
+++ geothermal/model_functions.py
@@ -1,8 +1,14 @@
 from enum import Enum
+import h5py
 import json
 import layermesh.mesh as lm
+import matplotlib.pyplot as plt
 import pywaiwera
 import yaml
+
+plt.rc("text", usetex=True)
+plt.rc("font", family="serif")
+
 
 class ExitFlag(Enum):
 
@@ -14,9 +20,9 @@ class ExitFlag(Enum):
 
 def build_base_model(
         xmax, ymax, zmax, nx, ny, nz,
+        mesh_name, model_name, model_folder,
         P_atm=1.0e+5, T_atm=20.0, P0=1.0e+5, T0=20.0, 
-        permeability=1.0e-14, porosity=0.25,
-        mesh_name="g2D", model_name="2D_base", model_folder="geothermal/models"
+        permeability=1.0e-14, porosity=0.25
     ):
 
     dx = xmax / nx
@@ -48,12 +54,16 @@ def build_base_model(
             "name" : f"{c.index}", 
             "permeability" : permeability, 
             "porosity" : porosity, 
-            "cells" : [c.index]
+            "cells" : [c.index],
+            "wet_conductivity" : 2.5,
+            "dry_conductivity" : 2.5,
+            "density" : 2.5e+3,
+            "specific_heat" : 1.0e+3
         }
         for c in mesh.cell
     ]}
 
-    inflow_cells = [mesh.column[n].cell[-1].index for n in [0, 9, 10, 11, 12, 13]]
+    inflow_cells = [mesh.column[n].cell[-1].index for n in [8, 9, 10, 11]]
 
     model["source"] = [{
         "component" : 1,
@@ -86,7 +96,7 @@ def build_base_model(
                 "minimum" : 5, 
                 "maximum" : 8
             }, 
-            "maximum" : {"number" : 500},
+            "maximum" : {"number" : 10_000},
             "method" : "beuler",
             "stop" : {"size" : {"maximum" : 1.0e+15}}
         }
@@ -94,6 +104,20 @@ def build_base_model(
 
     model["output"] = {"filename" : f"{model_folder}/{model_name}.h5"}
     model["logfile"] = {"echo" : False}
+
+    with open(f"{model_folder}/{model_name}.json", "w") as f:
+        json.dump(model, f, indent=2, sort_keys=True)
+
+
+def build_model(model_folder, base_model_name, model_name, permeabilities):
+
+    with open(f"{model_folder}/{base_model_name}.json", "r") as f:
+        model = json.load(f)
+
+    for rt in model["rock"]["types"]:
+        rt["permeability"] = permeabilities[rt["cells"][0]]
+
+    model["output"]["filename"] = f"{model_folder}/{model_name}.h5"
 
     with open(f"{model_folder}/{model_name}.json", "w") as f:
         json.dump(model, f, indent=2, sort_keys=True)
@@ -126,3 +150,29 @@ def run_info(log_fname):
 
     raise Exception("Unknown exit condition encountered. Check the log.")
     # return ExitFlag.unknown
+
+
+def slice_plot(
+        model_folder, mesh_name, quantity, 
+        value_label="log(Permeability)", value_unit="m$^2$"
+    ):
+
+    mesh = lm.mesh(f"{model_folder}/{mesh_name}.h5")
+
+    mesh.slice_plot(
+        value=quantity, 
+        value_label=value_label,
+        value_unit=value_unit,
+        colourmap="coolwarm",
+        xlabel="$x$ (m)",
+        ylabel="$y$ (m)"
+    )
+
+
+def get_temperatures(model_path):
+    
+    results = h5py.File(f"{model_path}.h5", "r")
+    index = results["cell_index"][:, 0]
+
+    temps = results["cell_fields"]["fluid_temperature"][-1][index]
+    return temps
--- geothermal/pycall_test.jl
+++ geothermal/pycall_test.jl
@@ -1,16 +1,52 @@
 using PyCall
+using SimIntensiveInference
 
 @pyinclude "geothermal/model_functions.py"
 
 model_folder = "geothermal/models"
 mesh_name = "g2D"
-model_name = "2D_base"
+base_model_name = "2D_base"
 
-# Define geometry parameters
 xmax, nx = 1000.0, 20
 ymax, ny = 50.0, 1
 zmax, nz = 1000.0, 20
 
-py"build_base_model"(xmax, ymax, zmax, nx, ny, nz)
-py"run_model"("$(model_folder)/$(model_name).json")
-flag = py"run_info"("$(model_folder)/$(model_name).yaml")
+dx = xmax / nx
+dz = zmax / nz
+
+xs = collect(range(dx, xmax-dx, nx))
+zs = collect(range(dz, zmax-dz, nz))
+
+py"build_base_model"(
+    xmax, ymax, zmax, nx, ny, nz, 
+    mesh_name, base_model_name, model_folder
+)
+
+# py"run_model"("$(model_folder)/$(model_name).json")
+# flag = py"run_info"("$(model_folder)/$(model_name).yaml")
+
+# Figure out what to do about clay cap 
+# Adapt ensemble methods to account for model failures
+# Extend things to production history
+
+logμ_p = -14.0
+σ_p = 0.5
+γ_p = 200
+k = ExpSquaredKernel(σ_p, γ_p)
+
+# Coordinate arrays swapped due to Layermesh convention
+p = GaussianPrior(logμ_p, k, zs, xs)
+
+logps = vec(rand(p))
+ps = 10 .^ logps
+
+py"build_model"(model_folder, base_model_name, "test_model", ps)
+
+@time py"run_model"("$(model_folder)/test_model.json")
+flag = py"run_info"("$(model_folder)/test_model.yaml")
+
+model_path = "$(model_folder)/test_model"
+
+temps = py"get_temperatures"(model_path)
+
+py"slice_plot"(model_folder, mesh_name, temps)
