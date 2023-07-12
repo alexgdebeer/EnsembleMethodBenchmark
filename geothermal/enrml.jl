f9f8bab56642c800d4e4c4b362c543581dad6740
alexgdebeer <adeb970@aucklanduni.ac.nz>
2023-07-12 13:22:05 +1200

    Update ensemble method scripts

--- geothermal/enrml.jl
+++ geothermal/enrml.jl
@@ -4,12 +4,9 @@ Ne = 25
 γ = 10
 i_max = 10
 
-θs, fs, us, Ss, λs, inds = run_lm_enrml(f, g, p, L, γ, i_max, Ne)
+θs, fs, us, ss, λs, inds = run_lm_enrml(f, g, p, L, γ, i_max, n_blocks, Ne)
 
-logps_post = reduce(hcat, get_perms(p, θ) for θ ∈ eachcol(θs[:,:,end][:,inds]))
+logps_post = reduce(hcat, get_perms(p, θ) for θ ∈ eachcol(θs[:,inds,end]))
 
-μ_post_x = reshape(mean(logps_post[1:n_blocks, :], dims=2), nx, nz)
-σ_post_x = reshape( std(logps_post[1:n_blocks, :], dims=2), nx, nz)
-
-μ_post_z = reshape(mean(logps_post[n_blocks+1:end, :], dims=2), nx, nz)
-σ_post_z = reshape( std(logps_post[n_blocks+1:end, :], dims=2), nx, nz)
+μ_post = reshape(mean(logps_post, dims=2), nx, nz)
+σ_post = reshape( std(logps_post, dims=2), nx, nz)
--- geothermal/mda.jl
+++ geothermal/mda.jl
@@ -1,13 +1,10 @@
 include("setup.jl")
 
-Ni = 4
-Ne = 50
+Ni = 8
+Ne = 100
 θs, fs, us, αs, inds = run_es_mda(f, g, p, L, n_blocks, Ni, Ne)#, α_method=:constant)
 
 logps_post = reduce(hcat, get_perms(p, θ) for θ ∈ eachcol(θs[:,inds,end]))
 
-μ_post_x = reshape(mean(logps_post[1:n_blocks, :], dims=2), nx, nz)
-σ_post_x = reshape( std(logps_post[1:n_blocks, :], dims=2), nx, nz)
-
-μ_post_z = reshape(mean(logps_post[n_blocks+1:end, :], dims=2), nx, nz)
-σ_post_z = reshape( std(logps_post[n_blocks+1:end, :], dims=2), nx, nz)
+μ_post = reshape(mean(logps_post, dims=2), nx, nz)
+σ_post = reshape( std(logps_post, dims=2), nx, nz)
