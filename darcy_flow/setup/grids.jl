df3a55cefd143d8e9a743866ebce8cd17e556ca6
alexgdebeer <adeb970@aucklanduni.ac.nz>
2023-10-03 15:05:54 +1300

    Update SteadyStateGrid struct

--- darcy_flow/setup/grids.jl
+++ darcy_flow/setup/grids.jl
@@ -13,6 +13,11 @@ struct SteadyStateGrid <: Grid
     Δx::Real
     Δy::Real
 
+    is_corner::AbstractVector 
+    is_bounds::AbstractVector 
+    bs_bounds::AbstractVector
+    is_inner::AbstractVector
+
     nx::Int
     ny::Int
     nu::Int
@@ -24,6 +29,27 @@ struct SteadyStateGrid <: Grid
         ys::AbstractVector,
         μ::Real=1.0,
     )::SteadyStateGrid 
+
+        function add_point_type!(i, x, y)
+
+            if x ∈ [xmin, xmax] && y ∈ [ymin, ymax]
+                push!(is_corner, i)
+                return
+            end
+
+            if x ∈ [xmin, xmax] || y ∈ [ymin, ymax]
+                x == xmin && push!(bs_bounds, :x0)
+                x == xmax && push!(bs_bounds, :x1)
+                y == ymin && push!(bs_bounds, :y0)
+                y == ymax && push!(bs_bounds, :y1)
+                push!(is_bounds, i)
+                return
+            end
+            
+            push!(is_inner, i)
+            return
+
+        end
         
         xmin, xmax = extrema(xs)
         ymin, ymax = extrema(ys)
@@ -35,11 +61,24 @@ struct SteadyStateGrid <: Grid
         ny = length(ys)
         nu = nx * ny
 
+        ixs = repeat(xs, outer=ny)
+        iys = repeat(ys, inner=nx)
+
+        is_corner = []
+        is_bounds = []
+        bs_bounds = []
+        is_inner  = []
+
+        for (i, (x, y)) ∈ enumerate(zip(ixs, iys))
+            add_point_type!(i, x, y)
+        end
+
         return new(
             xs, ys, 
             xmin, xmax, 
             ymin, ymax, 
             Δx, Δy, 
+            is_corner, is_bounds, bs_bounds, is_inner,
             nx, ny, nu,
             μ
         )
