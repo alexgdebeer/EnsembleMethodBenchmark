2bbf9db7bb9d395ee3b7ec94fdf97c6ca6e2bef1
alexgdebeer <adeb970@aucklanduni.ac.nz>
2023-10-25 16:23:04 +1300

    Update EKI and pCN scripts

--- run_eki.jl
+++ run_eki.jl
@@ -2,7 +2,7 @@ include("setup.jl")
 include("InferenceAlgorithms/eki.jl")
 
 NF = grid_c.nx^2 * grid_c.nt
-Ne = 100
+Ne = 1000
 
 ηs, Fs, Gs, αs = run_eki_dmc(F_r, G, pr, y_obs, μ_ε, Γ_e, NF, Ne)
 
@@ -10,3 +10,9 @@ lnps = hcat([transform(pr, η) for η in eachcol(ηs[end])]...)
 
 μ_post = reshape(mean(lnps, dims=2), grid_c.nx, grid_c.nx)
 σ_post = reshape(std(lnps, dims=2), grid_c.nx, grid_c.nx)
+
+# Hyperparameters
+σs_pri = [pr.σ_bounds[1] + cdf(Normal(), σ) * pr.Δσ for σ ∈ ηs[1][end-1, :]]
+ls_pri = [pr.l_bounds[1] + cdf(Normal(), l) * pr.Δl for l ∈ ηs[1][end, :]]
+σs_post = [pr.σ_bounds[1] + cdf(Normal(), σ) * pr.Δσ for σ ∈ ηs[end][end-1, :]]
+ls_post = [pr.l_bounds[1] + cdf(Normal(), l) * pr.Δl for l ∈ ηs[end][end, :]]
--- run_pcn.jl
+++ run_pcn.jl
@@ -3,9 +3,17 @@ include("InferenceAlgorithms/pcn.jl")
 
 NF = grid_c.nx^2 * grid_c.nt
 Ni = 500_000
-Nc = 1
 
 β = 0.05
 δ = 0.5
 
-ηs, Fs, Gs, τs = run_pcn(F_r, G, pr, y_obs, μ_ε, L_e, NF, Ni, Nc, β, δ, save_increment=100)
+chain_num = 1
+Nb = 100
+
+run_pcn(
+    F_r, G, pr, y_obs, 
+    μ_ε, L_e, 
+    NF, Ni, Nb,
+    β, δ, 
+    chain_num
+)
