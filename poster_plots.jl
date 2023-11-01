eea211bdac4486301abd5f23359b165737619143
alexgdebeer <adeb970@aucklanduni.ac.nz>
2023-11-01 21:19:47 +1300

    Remove old plotting functions

--- poster_plots.jl
+++ poster_plots.jl
@@ -1,5 +1,4 @@
 using HDF5
-using KernelDensity
 using LaTeXStrings
 using PyCall
 using PyPlot
@@ -14,6 +13,7 @@ include("setup.jl")
 FNAME_PRIOR = "data/prior/prior.h5"
 FNAME_MCMC = "data/pcn/pcn.h5"
 FNAME_EKI = "data/eki/eki.h5"
+FNAME_ENRML = "data/enrml/enrml.h5"
 FNAME_LAPLACE = "data/laplace/laplace.h5"
 
 TITLE_SIZE = 14
@@ -21,9 +21,12 @@ LABEL_SIZE = 8
 TEXT_SIZE = 10
 TICK_SIZE = 6
 
-POSTER_BACKGROUND = "lightgrey"
+POSTER_BACKGROUND = "gainsboro"
 POSTER_GREEN = "forestgreen" # TODO: fix
 POSTER_PURPLE = "rebeccapurple"
+
+CMAP_PERMS = :viridis
+CMAP_PRESSURES = :magma
 
 WELL_TO_PLOT = 4
 
@@ -57,7 +60,7 @@ function plot_forward_problem(well_centres, θ_t, u_t)
         axes[2, 2].clear()
 
         axes[2, 1].pcolormesh(grid_f.xs, grid_f.xs, u_t[:, :, i+1], 
-                              cmap=:turbo, vmin=u_min, vmax=u_max)
+                              cmap=CMAP_PRESSURES, vmin=u_min, vmax=u_max)
         
         set_reservoir_ticks!(axes[2, 1])
         axes[2, 1].set_title("Reservoir Pressure", fontsize=TITLE_SIZE)
@@ -76,7 +79,7 @@ function plot_forward_problem(well_centres, θ_t, u_t)
     end
 
     fig, axes = PyPlot.subplots(nrows=2, ncols=2, figsize=(5.75, 6))
-    fig.patch.set_facecolor("gainsboro")
+    fig.patch.set_facecolor(POSTER_BACKGROUND)
 
     # Plot well positions
     for (i, c) ∈ zip(well_nums, well_centres) 
@@ -85,7 +88,7 @@ function plot_forward_problem(well_centres, θ_t, u_t)
     end
     axes[1, 1].set_facecolor("lightskyblue")
 
-    axes[1, 2].pcolormesh(grid_f.xs, grid_f.xs, θ_t, cmap=:turbo)
+    axes[1, 2].pcolormesh(grid_f.xs, grid_f.xs, θ_t, cmap=CMAP_PERMS)
 
     for ax ∈ axes
         ax.set_box_aspect(1)
@@ -109,51 +112,6 @@ BATCH_LENGTH = 100
 
 BATCH_INC = 10
 BATCH_INDS = BATCH_INC:BATCH_INC:BATCH_LENGTH
-
-function plot_mcmc(n_batches::Int=500)
-
-    function make_frame(i::Int)
-
-        println(i)
-
-        axes[1, 1].set_title("Sample $i")        
-        sample_plot.set_data(samples[:, :, i+1])
-        cm_plot.set_data(cms[:, :, i+1])
-        # axes[1, 2].plot(ls[1:i+1], color="black")
-
-    end
-
-    fig, axes = PyPlot.subplots(nrows=2, ncols=2, figsize=(6, 6))
-    fig.patch.set_facecolor(POSTER_BACKGROUND)
-
-    axes[1, 2].set_title("Conditional Mean")
-
-    # Read in the MCMC output
-    f = h5open("data/mcmc/chain_1.h5", "r")
-    θs = reduce(hcat, [f["θs_$i"][:, end] for i ∈ 1:n_batches])
-    ls = reduce(vcat, [f["ηs_$i"][end, end] for i ∈ 1:n_batches])
-    close(f)
-
-    samples = θs[:, :]
-    cms = reduce(hcat, [mean(θs[:, 1:i], dims=2) for i ∈ 1:size(samples, 2)])
-    
-    samples = reshape(samples, grid_c.nx, grid_c.nx, :)
-    cms = reshape(cms, grid_c.nx, grid_c.nx, :)
-
-    θ_min, θ_max = extrema(samples)
-    sample_plot = axes[1, 1].imshow(samples[:, :, 1], cmap=:turbo, vmin=θ_min, vmax=θ_max)
-    cm_plot = axes[1, 2].imshow(cms[:, :, 1], cmap=:turbo, vmin=θ_min, vmax=θ_max)
-
-    # TODO: compute conditional means and standard deviations.
-
-    n_frames = size(samples, 3)
-    ani = anim.FuncAnimation(fig, make_frame, frames=n_frames, 
-                             interval=250)
-
-    ani.save(filename="mcmc.gif", writer="pillow", dpi=200, fps=5)
-
-
-end
 
 function plot_method_comparison(
     g::Grid, 
@@ -200,8 +158,8 @@ function plot_method_comparison(
         us_wa = get_well_pressures(f["Fs"][:, :], well_a)[:, 1:100]
         us_wb = get_well_pressures(f["Fs"][:, :], well_b)[:, 1:100]
 
-        axes[1, i].pcolormesh(g.xs, g.xs, μ, cmap=:turbo, vmin=μ_min, vmax=μ_max)
-        axes[2, i].pcolormesh(g.xs, g.xs, σ, cmap=:turbo, vmin=σ_min, vmax=σ_max)
+        axes[1, i].pcolormesh(g.xs, g.xs, μ, cmap=CMAP_PERMS, vmin=μ_min, vmax=μ_max)
+        axes[2, i].pcolormesh(g.xs, g.xs, σ, cmap=CMAP_PERMS, vmin=σ_min, vmax=σ_max)
 
         # axes[3, i].axvline(80, color="darkgray", linestyle="--", zorder=0)
         axes[3, i].plot(g.ts, us_wa, color=POSTER_GREEN, linewidth=1, zorder=1, alpha=0.25)
@@ -226,7 +184,7 @@ function plot_method_comparison(
         ax.set_yticks([])
     end
 
-    fnames = [FNAME_PRIOR, FNAME_MCMC, FNAME_LAPLACE, FNAME_EKI]
+    fnames = [FNAME_PRIOR, FNAME_MCMC, FNAME_LAPLACE, FNAME_EKI, FNAME_ENRML]
     for (i, fname) ∈ enumerate(fnames)
         plot_method_data!(fname, i)
     end
@@ -243,62 +201,11 @@ function plot_method_comparison(
     axes[4, 1].set_ylabel("Predictions\nat Well 7", fontsize=TITLE_SIZE) # TODO: generalise
 
     PyPlot.tight_layout()
-    PyPlot.savefig("plots/poster/comparison.pdf")
+    PyPlot.savefig("plots/poster/comparison.png", dpi=300)
 
 end
 
-
-function plot_marginals(
-    g::Grid
-)
-
-    function plot_method_data!(
-        algname::AbstractString,
-        fname::AbstractString
-    )
-
-        f = h5open(fname, "r")
-        ls = f["l"][:]
-        θis = f["θi"][:]
-        close(f)
-
-        kde_l = kde(ls, boundary=pr.l_bounds)
-        kde_θi = kde(θis)
-
-        # TODO: plot the truth
-        axes[1].plot(kde_l.x, kde_l.density, label=algname)
-        #axes[2].plot(kde_θi.x, kde_θi.density, label=algname)
-        #axes[1].hist(ls, bins=30, density=true, label=algname, alpha=0.2)
-        #axes[2].hist(θis, bins=30, density=true, label=algname, alpha=0.2)
-
-    end
-
-    fig, axes = PyPlot.subplots(nrows=1, ncols=2, figsize=(6, 3))
-
-    for ax ∈ axes
-        ax.set_box_aspect(1)
-        ax.set_xticks([])
-        ax.set_yticks([])
-    end
-
-    fnames = [FNAME_PRIOR, FNAME_MCMC, FNAME_LAPLACE, FNAME_EKI]
-    algnames = ["Prior", "MCMC", "LMAP", "EKI"]
-    for (algname, fname) ∈ zip(algnames, fnames)
-        plot_method_data!(algname, fname)
-    end
-
-    axes[1].set_title("Lengthscale", fontsize=TITLE_SIZE)
-    axes[2].set_title("Permeability", fontsize=TITLE_SIZE)
-    axes[1].set_xlim(pr.l_bounds)
-    axes[1].legend()
-    axes[2].legend()
-
-    PyPlot.tight_layout()
-    PyPlot.savefig("plots/poster/marginals.pdf")
-
-end
-
-# plot_forward_problem(well_centres, θ_t, u_t)
+plot_forward_problem(well_centres, θ_t, u_t)
 # plot_mcmc()
 plot_method_comparison(grid_c, pr, n_wells, t_obs, d_obs)
 # plot_marginals(grid_c)
