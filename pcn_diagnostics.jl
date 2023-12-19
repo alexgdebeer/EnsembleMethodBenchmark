3b168d50d207fb0cf4775ce170944980ac3b4fc7
alexgdebeer <adeb970@aucklanduni.ac.nz>
2023-12-19 15:35:47 +1300

    Update pCN diagnostics script

--- pcn_diagnostics.jl
+++ pcn_diagnostics.jl
@@ -3,16 +3,11 @@ using Statistics
 
 include("setup.jl")
 
-CHAIN_LENGTH = 500_000 
-WARMUP_LENGTH = 5000
-BATCH_LENGTH = 100
-
-BATCH_INC = 10
-BATCH_INDS = BATCH_INC:BATCH_INC:BATCH_LENGTH
-
-N_CHAINS = 4
-N_BATCHES = CHAIN_LENGTH ÷ BATCH_LENGTH
-N_SAMPLES = length(BATCH_INDS) * N_BATCHES
+N_CHAINS = 5
+N_BATCHES = 20_000
+N_SAMPLES_PER_BATCH = 1
+N_SAMPLES = N_BATCHES * N_SAMPLES_PER_BATCH
+WARMUP_LENGTH = 500
 
 DATA_FOLDER = "data/pcn"
 RESULTS_FNAME = "data/pcn/pcn.h5"
@@ -27,7 +22,8 @@ function compute_psrf(
     
     μ = mean(θs)
     μ_js = [mean(c) for c ∈ eachcol(θs)]
-    s_js = [1 / (n-1) * sum((c .- μ_js[j]).^2) for (j, c) ∈ enumerate(eachcol(θs))]
+    s_js = [1 / (n-1) * sum((c .- μ_js[j]).^2) 
+            for (j, c) ∈ enumerate(eachcol(θs))]
 
     B = n / (m-1) * sum((μ_js .- μ).^2)
     W = 1 / m * sum(s_js)
@@ -41,38 +37,39 @@ end
 
 NF = n_wells * grid_c.nt
 
-ηs = zeros(pr.Nη, N_SAMPLES, N_CHAINS)
 θs = zeros(pr.Nθ, N_SAMPLES, N_CHAINS)
+us = zeros(pr.Nu, N_SAMPLES, N_CHAINS)
 Fs = zeros(NF, N_SAMPLES, N_CHAINS)
-τs = zeros(CHAIN_LENGTH, N_CHAINS)
+τs = zeros(100N_BATCHES, N_CHAINS)
 
 for i ∈ 1:N_CHAINS
 
     f = h5open("$(DATA_FOLDER)/chain_$i.h5", "r")
-    ηs[:, :, i] = reduce(hcat, [f["ηs_$b"][:, BATCH_INDS] for b ∈ 1:N_BATCHES])
-    θs[:, :, i] = reduce(hcat, [f["θs_$b"][:, BATCH_INDS] for b ∈ 1:N_BATCHES])
-    Fs[:, :, i] = reduce(hcat, [f["Fs_$b"][:, BATCH_INDS] for b ∈ 1:N_BATCHES])
-    τs[:, i] = reduce(vcat, [f["τs_$b"][:] for b ∈ 1:N_BATCHES])
+    θs[:, :, i] = reduce(hcat, [f["ηs_$b"][:, 1] for b ∈ 1:N_BATCHES])
+    us[:, :, i] = reduce(hcat, [f["θs_$b"][:, 1] for b ∈ 1:N_BATCHES])
+    Fs[:, :, i] = reduce(hcat, [f["Fs_$b"][:, 1] for b ∈ 1:N_BATCHES])
+    τs[:, i] = reduce(vcat, [f["τs_$b"][:, 1] for b ∈ 1:N_BATCHES])
     close(f)
     @info "Finished reading data from chain $i."
 
 end
 
-psrfs_η = @time [compute_psrf(ηs[i, WARMUP_LENGTH+1:end, :]) for i ∈ 1:pr.Nη]
-psrfs_θ = @time [compute_psrf(θs[i, WARMUP_LENGTH+1:end, :]) for i ∈ 1:pr.Nθ]
+psrfs_θ = [compute_psrf(θs[i, WARMUP_LENGTH+1:end, :]) for i ∈ 1:pr.Nθ]
 
-Fs = reshape(Fs, NF, :, 1)
-Fs = dropdims(Fs, dims=3)
+μ_post_θ = mean(reshape(θs[:, WARMUP_LENGTH+1:end, :], pr.Nθ, :, 1), dims=2)
+μ_post = reshape(transform(pr, vec(μ_post_θ)), grid_c.nx, grid_c.nx)
 
-μ_post_η = vec(mean(reshape(ηs[:, WARMUP_LENGTH+1:end, :], pr.Nη, :, 1), dims=2))
-μ_post = reshape(transform(pr, μ_post_η), grid_c.nx, grid_c.nx)
-σ_post = reshape(std(reshape(θs[:, WARMUP_LENGTH+1:end, :], pr.Nθ, :, 1), dims=2), grid_c.nx, grid_c.nx)
+σ_post = std(reshape(us[:, WARMUP_LENGTH+1:end, :], pr.Nu, :, 1), dims=2)
+σ_post = reshape(σ_post, grid_c.nx, grid_c.nx)
 
-θis = vec(θs[3200, WARMUP_LENGTH+1:end, :])
-ls = [pr.l_bounds[1] + cdf(Normal(), l) * pr.Δl for l ∈ vec(ηs[end, WARMUP_LENGTH+1:end, :])]
+trace_1 = τs[1:10:end, :]
+trace_2 = θs[2109, :, :]
+trace_3 = θs[end, :, :]
 
-h5write(RESULTS_FNAME, "Fs", Fs[:, 200:200:end]) # TODO: tidy
-h5write(RESULTS_FNAME, "μ", μ_post)
-h5write(RESULTS_FNAME, "σ", σ_post)
-h5write(RESULTS_FNAME, "θi", θis)
-h5write(RESULTS_FNAME, "l", ls)
+h5write(RESULTS_FNAME, "trace_1", trace_1)
+h5write(RESULTS_FNAME, "trace_2", trace_2)
+h5write(RESULTS_FNAME, "trace_3", trace_3)
+h5write(RESULTS_FNAME, "mean", μ_post)
+h5write(RESULTS_FNAME, "stds", σ_post)
+h5write(RESULTS_FNAME, "samples_1", us[:, 5_000, :])
+h5write(RESULTS_FNAME, "samples_2", us[:, 5_000, :])
