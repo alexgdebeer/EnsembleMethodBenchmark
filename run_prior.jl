3176328812df22143eb3cea6079ee1388db978f8
Alex de Beer <adeb970@aucklanduni.ac.nz>
2024-02-07 19:07:25 +1300

    Update script for sampling from prior

--- run_prior.jl
+++ run_prior.jl
@@ -5,8 +5,7 @@ n_samples = 1000
 
 θs = rand(pr, n_samples)
 us = hcat([transform(pr, θ_i) for θ_i ∈ eachcol(θs)]...)
-Fs = hcat([F(u_i) for u_i ∈ eachcol(us)]...)
-Gs = hcat([G(F_i) for F_i ∈ eachcol(Fs)]...)
+Fs = model_r.B_wells * hcat([F(u_i) for u_i ∈ eachcol(us)]...)
 
 μ_θ = mean(θs, dims=2)
 μ_pri = reshape(transform(pr, μ_θ), grid_c.nx, grid_c.nx)
@@ -16,4 +15,4 @@ h5write(fname, "mean", μ_pri)
 h5write(fname, "stds", σ_pri)
 h5write(fname, "θs", θs)
 h5write(fname, "us", us)
-h5write(fname, "Gs", Gs)
+h5write(fname, "Fs", Fs)
