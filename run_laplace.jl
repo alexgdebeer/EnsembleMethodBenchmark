e6e1339faea5448849c3ec5a62ea64d6dcb3e213
Alex de Beer <adeb970@aucklanduni.ac.nz>
2024-02-07 18:57:02 +1300

    Update LMAP script

--- run_laplace.jl
+++ run_laplace.jl
@@ -4,36 +4,42 @@ include("InferenceAlgorithms/InferenceAlgorithms.jl")
 Ne = 1000
 n_trials = 10
 
-fname = "data/laplace/laplace_$Ne.h5"
-
-results = Dict()
+results = Dict(100 => Dict(), 1000 => Dict())
 
 for i ∈ 1:n_trials 
-    
+        
     θ0 = vec(rand(pr, 1))
 
     map, Γ_post, L_post, n_solves = compute_laplace(
         grid_c, model_r, pr, d_obs, θ0
-    ) # TODO: fix θ -- it is returned as a sparse vector
+    )
 
-    θs = map.θ .+ L_post * rand(Normal(), pr.Nθ, Ne)
-    us = hcat([transform(pr, θi) for θi ∈ eachcol(θs)]...)
-    Fs = hcat([F(u_i) for u_i ∈ eachcol(us)]...)
-    Gs = hcat([G(F_i) for F_i ∈ eachcol(Fs)]...)
+    for r ∈ keys(results)
 
-    μ_post = reshape(Vector(map.u), grid_c.nx, grid_c.nx)
-    σ_post = reshape(std(us, dims=2), grid_c.nx, grid_c.nx)
+        θs = map.θ .+ L_post * rand(Normal(), pr.Nθ, r)
+        us = hcat([transform(pr, θi) for θi ∈ eachcol(θs)]...)
+        Fs = model_r.B_wells * hcat([F(u_i) for u_i ∈ eachcol(us)]...)
+        ls = [gauss_to_unif(ω_l, l_bounds...) for ω_l ∈ θs[end, :]]
 
-    results["θs_$i"] = Matrix(θs)
-    results["us_$i"] = us
-    results["Gs_$i"] = Gs
+        μ_post = reshape(Vector(map.u), grid_c.nx, grid_c.nx)
+        σ_post = reshape(std(us, dims=2), grid_c.nx, grid_c.nx)
+
+        results[r]["θs_$i"] = Matrix(θs)
+        results[r]["us_$i"] = us
+        results[r]["Fs_$i"] = Fs
+        results[r]["ls_$i"] = ls
     
-    results["μ_post_$i"] = μ_post
-    results["σ_post_$i"] = σ_post
+        results[r]["μ_post_$i"] = μ_post
+        results[r]["σ_post_$i"] = σ_post
     
-    results["n_its_$i"] = 1
-    results["n_sims_$i"] = n_solves + Ne
+        results[r]["n_its_$i"] = 1
+        results[r]["n_sims_$i"] = n_solves + r
+
+    end
 
 end
 
-save_results(results, fname)
+for r ∈ keys(results)
+    fname = "data/laplace/laplace_$(r).h5"
+    save_results(results[r], fname)
+end
